a838d6c7c2f5c851301e7a2fcb4ae1c84b7ea393
DESKTOP-G7VBR0R\WEIMOB <zhigang.zhang@weimob.com>
2022-06-07 13:38:32 +0800

    11

--- utilsMy/__env.js
+++ utilsMy/__env.js
@@ -32,12 +32,13 @@ const envList = {
   ZJD_OPEN: 20,
   jd_zdjr_activityId: '',
   jd_zdjr_activityUrl: '',
-  jd_wxSecond_activityId: '',
+  jd_wxSecond_activityId: 'e1282873c48e44ffba7396399e21e931',
   jd_wxCollectCard_activityId: '',
   tytpacketId: '',
   WindfggToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY1NDQyMTg4NiwianRpIjoiMWExZGYxYTYtMWIxOS00MGI5LWE1YWItZjBlZjk1NTk4MzE5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjE4MTAxNTYzNDAiLCJuYmYiOjE2NTQ0MjE4ODYsImV4cCI6MTY4NTk1Nzg4Nn0.EUVHSeOmhO4VTvf1Y-Gy-IDT_aOtNZPAokgBsl5q2A8',
   jd_cjhy_activityId: '17dba7a31860460fb34d14a6aea2b8e9',
-  jd_cjhy_activityUrl: 'https://cjhydz-isv.isvjcloud.com'
+  jd_cjhy_activityUrl: 'https://cjhydz-isv.isvjcloud.com',
+  jd_fxyl_activityId: '31cb3868a5f04dac86337cf4b242a28e'
 }
 
 function setEnv() {
