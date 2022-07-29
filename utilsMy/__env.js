dc66e35632bfad58c85262049fd536086bc47b62
DESKTOP-G7VBR0R\WEIMOB <zhigang.zhang@weimob.com>
2022-07-29 10:25:54 +0800

    11

--- utilsMy/__env.js
+++ utilsMy/__env.js
@@ -21,7 +21,7 @@ const envList = {
   JOY_COIN_MAXIMIZE: '1',
   dlbtz: true,
   jdPetNotNotifyList: ['lianhua891124', 'jd_45b7686c80a27', '%E9%A6%99%E7%8C%AA%E5%AE%9D', '13016482674_p', 'jd_58b8a58a68265'],
-  jdFruitNotNotifyList: ['%E9%A6%99%E7%8C%AA%E5%AE%9D', 'jd_OZSaXyfdZDNC', 'jd_6ddb4dbb04000', 'jd_68b0ef6313ac4', 'lianhua891124'],
+  jdFruitNotNotifyList: ['%E9%A6%99%E7%8C%AA%E5%AE%9D', 'jd_OZSaXyfdZDNC', 'jd_6ddb4dbb04000', 'jd_68b0ef6313ac4', '13016482674_p'],
   JD_JOY_PARK_RUN_MSG_TIME: [12, 16, 18, 20, 23],
   JD_JOY_PARK_RUN_ASSETS: '0.01',
   JD_JOY_PARK_RUN_ASSETS2: '0.03',
